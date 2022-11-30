3615be08cd175f2dd0ed8403498bc1852d24c9c3
Tracks2DevOps <jacksont116@gmail.com>
2022-11-30 13:17:51 -0800

    Created repo for network connections and automation. First script to connect to device via serial and NetMiko and establish ssh connection. Non-working version needs to fix name inputted from variable not matching required format?

--- serial_connect.py
+++ serial_connect.py
@@ -7,8 +7,13 @@ from netmiko import ConnectHandler
 
 serial_conn = subprocess.check_output(['ls /dev/ | grep "cu.usbserial"'], shell=True, text=True)
 print(serial_conn)
-serial_port = "/dev/" + serial_conn
-str(print(serial_port)).strip()
+serial_port2 = "/dev/" + serial_conn
+#str(serial_port2).strip()
+print(serial_port2)
+serial_port = "/dev/cu.usbserial-A9CB9ZHA"
+serial_variable = type(serial_port)
+print("The variable serial_port is: ", type(serial_port))
+print("The variable serial_port2 is: ", type(serial_port2))
 
 device = {
     "device_type": "cisco_ios_serial",
@@ -22,7 +27,7 @@ device = {
         "bytesize": serial.EIGHTBITS,
         "parity": serial.PARITY_NONE,
         "stopbits": serial.STOPBITS_ONE,
-        "port": serial_port,    # name that prints matches requested name but throws error
+        "port": serial_port2,    # name that prints matches requested name but throws error
     },
 }
 
@@ -34,3 +39,25 @@ with ConnectHandler(**device) as conn:
         output = conn.send_command("show version")
     output = conn.send_command("show version")
 print(output)
+
+"""
+Connecting...
+Traceback (most recent call last):
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/netmiko/utilities.py", line 247, in check_serial_port
+    cdc = next(serial.tools.list_ports.grep(name))
+StopIteration
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "/Users/travis.jackson/PycharmProjects/net_scripts/serial_connect.py", line 30, in <module>
+    with ConnectHandler(**device) as conn:
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/netmiko/ssh_dispatcher.py", line 365, in ConnectHandler
+    return ConnectionClass(*args, **kwargs)
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/netmiko/base_connection.py", line 396, in __init__
+    comm_port = check_serial_port(comm_port)
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/netmiko/utilities.py", line 257, in check_serial_port
+    raise ValueError(msg)
+ValueError: dev/cu.usbserial-A9CB9ZHA
+ not found. available devices are: /dev/cu.Bluetooth-Incoming-device /Port - n/a,/dev/cu.usbserial-A9CB9ZHA - FT232R USB UART,
+ """
--- serial_connect2.py
+++ serial_connect2.py
@@ -3,6 +3,8 @@
 import serial
 from netmiko import ConnectHandler
 
+# Establish serial connection settings
+
 device = {
     "device_type": "cisco_ios_serial",
     "username": "cisco",
@@ -19,6 +21,8 @@ device = {
     },
 }
 
+# Connect to device and verify by performing the show version command
+
 print("Connecting...")
 with ConnectHandler(**device) as conn:
     print("Connected")
@@ -26,4 +30,60 @@ with ConnectHandler(**device) as conn:
         conn.enable()
         output = conn.send_command("show version")
     output = conn.send_command("show version")
-print(output)
+print(output)   # Capture success or failure instead of printing to the screen
+
+# Enable a default remote connection for advanced configurations/automation
+"""
+- Connect to the vlan
+    'conf t'
+    'interface vlan 1'
+- Set a management IP/subnet
+- Set the default gateway
+- Assign hostname and domain name?
+- Set user and password
+- Enable SSH
+- Test remote connection
+"""
+
+default_mgmt= [
+    'conf t',
+    'hostname lasC3560X-test',
+    'ip domain-name lasC3560X-test.com',
+    'username techno privilege 15 secret test123',
+    'crypto key generate rsa',
+    '2048',
+    'ip ssh version 2',
+    'vlan 128',
+    'interface vlan 128',
+    'ip address 10.9.128.99 255.255.255.0',
+    'int gi0/47',
+    'switchport mode access',
+    'switchport access vlan 128',
+    'no shut',
+    'exit'
+
+]
+
+def_mgmt_conf = conn.send_config_set(default_mgmt)
+def_mgmt_output = conn.send_command("show ip arp")
+print(def_mgmt_output)
+conn.disconnect()
+
+r_device = {
+'device_type': 'cisco_ios',
+    'host': '10.9.128.99',
+    'username': 'cisco',
+    'password': 'cisco',
+    'port': 20022,
+}
+
+print("Connecting...")
+with ConnectHandler(**r_device) as ssh:
+    print("Connected")
+    if not ssh.check_enable_mode():
+        ssh.enable()
+        ssh_output = ssh.send_command("show version")
+    ssh_output = ssh.send_command("show version")
+print(ssh_output)
+ssh.disconnect()
+# Create Customer's environment
